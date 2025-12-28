747f47a116557c84801c107ff9912c44e60c2e26
asurd <how133@outlook.com>
2025-12-28 23:28:28 +0800

    first commit

---
+++ Source/MPTesting/MPTesting.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MPTesting : ModuleRules
+{
+	public MPTesting(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"MPTesting",
+			"MPTesting/Variant_Horror",
+			"MPTesting/Variant_Horror/UI",
+			"MPTesting/Variant_Shooter",
+			"MPTesting/Variant_Shooter/AI",
+			"MPTesting/Variant_Shooter/UI",
+			"MPTesting/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
